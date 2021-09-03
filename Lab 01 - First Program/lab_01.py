f0321d89f2ddacb7c04779210b1f242d4424cfbc
Ethan Larbi <ethan.larbi@my.simpson.edu>
2021-09-03 17:08:27 -0500

    lab 1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,7 @@
+print("hi"
+      "")
+print ("Hello, my name is Ethan Larbi.")
+print ("I like green turtles with shells!")
+print ("Coding is a cool class to be in.")
+print ("Film photography is a dying work of art."
+       "")
