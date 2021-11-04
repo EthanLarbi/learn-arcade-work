3b2b36e4c50c0fcc1344a0fbd413177e19776bdf
Ethan Larbi <ethan.larbi@my.simpson.edu>
2021-11-04 14:38:06 -0500

    lab 8

--- Testing/test.py
+++ Testing/test.py
@@ -1,94 +1,107 @@
+""" Sprite Sample Program """
+
 import arcade
 
-SCREEN_WIDTH = 640
-SCREEN_HEIGHT = 480
-MOVEMENT_SPEED = 3
+# --- Constants ---
+SPRITE_SCALING_BOX = 0.5
+SPRITE_SCALING_PLAYER = 0.5
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
-class Ball:
-    def __init__(self, position_x, position_y, change_x, change_y, radius, color):
-
-        # Take the parameters of the init function above,
-        # and create instance variables out of them.
-        self.position_x = position_x
-        self.position_y = position_y
-        self.change_x = change_x
-        self.change_y = change_y
-        self.radius = radius
-        self.color = color
-
-    def draw(self):
-        """ Draw the balls with the instance variables we have. """
-        arcade.draw_circle_filled(self.position_x,
-                                  self.position_y,
-                                  self.radius, self.
-                                  color)
-
-    def update(self):
-        # Move the ball
-        self.position_y += self.change_y
-        self.position_x += self.change_x
-
-        # See if the ball hit the edge of the screen. If so, change direction
-        if self.position_x < self.radius:
-            self.position_x = self.radius
-
-        if self.position_x > SCREEN_WIDTH - self.radius:
-            self.position_x = SCREEN_WIDTH - self.radius
-
-        if self.position_y < self.radius:
-            self.position_y = self.radius
-
-        if self.position_y > SCREEN_HEIGHT - self.radius:
-            self.position_y = SCREEN_HEIGHT - self.radius
+MOVEMENT_SPEED = 5
 
 
 class MyGame(arcade.Window):
+    """ This class represents the main window of the game. """
+    """ Sprite Sample Program """
 
-    def __init__(self, width, height, title):
+    import arcade
 
-        # Call the parent class's init function
-        super().__init__(width, height, title)
+    # --- Constants ---
+    SPRITE_SCALING_BOX = 0.5
+    SPRITE_SCALING_PLAYER = 0.5
 
-        # Make the mouse disappear when it is over the window.
-        # So we just see our object, not the pointer.
-        self.set_mouse_visible(False)
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 600
 
-        arcade.set_background_color(arcade.color.ASH_GREY)
+    MOVEMENT_SPEED = 5
+    """ Sprite Sample Program """
 
-        # Create our ball
-        self.ball = Ball(50, 50, 0, 0, 15, arcade.color.AUBURN)
+    import arcade
+
+    # --- Constants ---
+    SPRITE_SCALING_BOX = 0.5
+    SPRITE_SCALING_PLAYER = 0.5
+
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 600
+
+    MOVEMENT_SPEED = 5
+
+    class MyGame(arcade.Window):
+        """ This class represents the main window of the game. """
+
+        def __init__(self):
+            """ Initializer """
+            # Call the parent class initializer
+            super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprites With Walls Example")
+
+        def setup(self):
+            # Set the background color
+            arcade.set_background_color(arcade.color.AMAZON)
+
+        def on_draw(self):
+            arcade.start_render()
+
+    def main():
+        window = MyGame()
+        window.setup()
+        arcade.run()
+
+    if __name__ == "__main__":
+        main()
+    class MyGame(arcade.Window):
+        """ This class represents the main window of the game. """
+
+        def __init__(self):
+            """ Initializer """
+            # Call the parent class initializer
+            super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprites With Walls Example")
+
+        def setup(self):
+            # Set the background color
+            arcade.set_background_color(arcade.color.AMAZON)
+
+        def on_draw(self):
+            arcade.start_render()
+
+    def main():
+        window = MyGame()
+        window.setup()
+        arcade.run()
+
+    if __name__ == "__main__":
+        main()
+    def __init__(self):
+        """ Initializer """
+        # Call the parent class initializer
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprites With Walls Example")
+
+
+    def setup(self):
+        # Set the background color
+        arcade.set_background_color(arcade.color.AMAZON)
 
     def on_draw(self):
-        """ Called whenever we need to draw the window. """
         arcade.start_render()
-        self.ball.draw()
-
-    def update(self, delta_time):
-        self.ball.update()
-
-    def on_key_press(self, key, modifiers):
-        """ Called whenever the user presses a key. """
-        if key == arcade.key.LEFT:
-            self.ball.change_x = -MOVEMENT_SPEED
-        elif key == arcade.key.RIGHT:
-            self.ball.change_x = MOVEMENT_SPEED
-        elif key == arcade.key.UP:
-            self.ball.change_y = MOVEMENT_SPEED
-        elif key == arcade.key.DOWN:
-            self.ball.change_y = -MOVEMENT_SPEED
-
-    def on_key_release(self, key, modifiers):
-        """ Called whenever a user releases a key. """
-        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
-            self.ball.change_x = 0
-        elif key == arcade.key.UP or key == arcade.key.DOWN:
-            self.ball.change_y = 0
 
 
 def main():
-    window = MyGame(640, 480, "Drawing Example")
+    window = MyGame()
+    window.setup()
     arcade.run()
 
 
-main()
+if __name__ == "__main__":
+    main()
